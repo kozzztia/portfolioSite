ec23fb8fa7d63ac76de31fcf008476eb3aa5486e
kozzztia <kozzztia@ukr.net>
2021-11-13 16:45:49 +0200

    two

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,22 @@
-$(document).ready(function(){
-  let go = prompt('Please enter your namekostia')
-
-  alert('hello ' + go)
-
-
+// $(document).ready(function(){
+    let num = document.querySelector('.input');
+    let monitor = document.querySelector('.monitor');
+    let text = document.querySelector('.text');
 
 
+  monitor.innerHTML= 'number ' + num.value;
+  num.oninput = function(){
+    monitor.innerHTML = 'number ' + this.value;
+    if(num.value == 10){
+      monitor.innerHTML = 'The end ';
+    }
+    text.style.fontSize = this.value*3 +'px';
+  }
 
 
 
-  });
+
+
+
+
+  // });
